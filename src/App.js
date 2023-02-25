ae5c5f228496a2c7135c2b371bca1be4b3aa23bf
Hitesh Tiwari <72449162+hit98gag@users.noreply.github.com>
2023-02-25 19:17:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import  { useState } from 'react'
+import Header from './Components/Header'
+import Theme from './Components/Theme'
 function App() {
+  const [darktheme, setDarktheme] = useState(false)
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header theme={darktheme} setDarktheme={setDarktheme}/>
+    <Theme theme={darktheme}/>
+  
+      </div>
   );
 }
 
 export default App;
+
+
+
